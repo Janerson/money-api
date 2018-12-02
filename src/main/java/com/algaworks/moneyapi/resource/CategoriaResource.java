7f1fa2a86d6cdaf9a803003bbaba9e7dcba36bc5
Janerson Douglas <douglas.janerson@gmail.com>
2018-12-01 23:19:25 -0200

    adicionado model, controller e repository para categoria

---
+++ src/main/java/com/algaworks/moneyapi/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.algaworks.moneyapi.resource;
+
+import com.algaworks.moneyapi.model.Categoria;
+import com.algaworks.moneyapi.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return categoriaRepository.findAll();
+    }
+}
