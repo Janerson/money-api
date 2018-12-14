767620f05ff44603b7a12404591b2934488c49da
Janerson Douglas <douglas.janerson@gmail.com>
2018-12-13 22:28:30 -0200

    migração tabela lancamentos

--- src/main/java/com/algaworks/moneyapi/repository/CategoriaRepository.java
+++ src/main/java/com/algaworks/moneyapi/repository/CategoriaRepository.java
@@ -2,9 +2,14 @@ package com.algaworks.moneyapi.repository;
 
 import com.algaworks.moneyapi.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
-
+    @Query("from Categoria c where  c.nome like %:nome%")
+    List<Categoria> listarBy(@Param("nome") String param);
 
 }
---
+++ src/main/resources/db/migration/V03__criar_e_registrar_tabela_lancamentos.sql
@@ -0,0 +1,15 @@
+CREATE TABLE lancamentos(
+  codigo bigint(20) PRIMARY KEY AUTO_INCREMENT,
+  descricao varchar(50) not null,
+  data_vencimento date not null,
+  data_pagamento date,
+  valor decimal(10,2) not null,
+  observacao varchar(100),
+  tipo varchar (20) not null,
+  codigo_categoria bigint(20) not null,
+  codigo_pessoa bigint(20) not null,
+  foreign key (codigo_categoria) references categorias(codigo),
+  foreign key (codigo_pessoa) references pessoas(codigo)
+)ENGINE=InnoDB default CHARSET=utf8;
+
+insert into lancamentos(descricao,data_vencimento,data_pagamento,valor,observacao,tipo,codigo_categoria,codigo_pessoa)values ('PAGAMENTO FARMACIA','2018-12-13','2019-02-01',150.80,'Nao esquecer de cobrar','DESPESA',3,1);
